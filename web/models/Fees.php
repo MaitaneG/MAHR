254b14008ca3448675a8fa6af24a2bd23175b6ef
Ruben Santibanez <santibanez.ruben@uni.eus>
2021-05-07 14:17:29 +0200

    web models finished

--- web/models/Fees.php
+++ web/models/Fees.php
@@ -1,2 +1,71 @@
+
 <?php
+
+include("testConexion.php");
+
+//PRODUCTIONS TABLE API
+function selectFeesMail($mail) {
+    //Select total fees list for a mail
+    //return: 
+    //0-id_fee 1-year 2-mail 3-payed
+    $conexion = ConnectDataBase();
+    $query = "SELECT * FROM fees where mail='$mail'";
+    $result = mysqli_query($conexion, $query);
+    if (!$result) {
+        die("Query error" . mysqli_error($conexion));
+    }
+    $results = array();
+    while ($row = mysqli_fetch_array($result)) {
+        $results[] = array(
+            'id_fee' => $row[0],
+            'year' => $row[1],
+            'mail' => $row[2],
+            'payed' => $row[3]
+        );
+    }
+    mysqli_free_result($row);
+    mysqli_close($conexion);
+    return $results;
+}
+
+function selectFeeID($id) {
+    //Select a fee for Id
+    //return: 
+    //0-id_fee 1-year 2-mail 3-payed
+    $conexion = ConnectDataBase();
+    $query = "SELECT * FROM productions WHERE id_fee='$id";
+
+    $result = mysqli_query($conexion, $query);
+    if (!$result) {
+        die("Query error" . mysqli_error($conexion));
+    }
+    $results = array();
+    while ($row = mysqli_fetch_array($result)) {
+        $results[] = array(
+            'id_fee' => $row[0],
+            'year' => $row[1],
+            'mail' => $row[2],
+            'payed' => $row[3]
+        );
+    }
+    mysqli_free_result($row);
+    mysqli_close($conexion);
+    return $results;
+}
+
+function updateFeePayed($id,$payed) {
+    //Add a new reserve
+    //return String
+    include("connect.php");
+    $conexion = ConnectDataBase();
+
+    $query = "UPDATE fees SET payed=$payed where id_fee=$id";
+    $result = mysqli_query($conexion, $query);
+    mysqli_free_result($row);
+    mysqli_close($conexion);
+    if (!$result) {
+        return "SERVER ERROR";
+    }
+    return "reserve done";
+}
 
--- web/models/Productions.php
+++ web/models/Productions.php
@@ -55,7 +55,7 @@ function selectProductionDate($date) {
     return $results;
 }
 
-function updateProduction($date, $mail) {
+function insertProduction($date, $mail) {
     //Add a new reserve
     //return String
     include("connect.php");
@@ -71,19 +71,3 @@ function updateProduction($date, $mail) {
     return "reserve done";
 }
 
-function deleteReserve($date, $mail) {
-    //Delete a reserve
-    //return string
-    include("connect.php");
-    $conexion = ConnectDataBase();
-
-    $query = "DELETE FROM bookings where date=$date and mail=$mail";
-    $result = mysqli_query($conexion, $query);
-    mysqli_free_result($row);
-    mysqli_close($conexion);
-    if (!$result) {
-
-        return "NO MATCH";
-    }
-    return "reserve deleted";
-}
