3cb35526ccc5e0b3f666f70cb1ce08b804da353d
Moreno Rodriguez <moreno.manuel@uni.lan>
2021-05-10 09:46:57 +0200

    Merge

---
+++ ErleteAdministration/src/classes/Container_Merge.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+import java.time.LocalDate;
+
+public class Container_Merge {
+    private int id;
+    private int capacity;
+    private LocalDate date;
+    private String email;
+
+    public Container_Merge(int id, int capacity, String date, String email) {
+        this.id = id;
+        this.capacity = capacity;
+        this.date = LocalDate.parse(date);
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+    
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setDate(String date) {
+        this.date = LocalDate.parse(date);
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
--- ErleteAdministration/src/tableModels/CansTableModel.java
+++ ErleteAdministration/src/tableModels/CansTableModel.java
@@ -5,10 +5,64 @@
  */
 package tableModels;
 
+import Classes.Container_Merge;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+import mvc.Model;
+
 /**
  *
  * @author gallastegui.maitane
  */
-public class CansTableModel {
+public class CansTableModel extends AbstractTableModel{
+    private Model model = new Model();
+    private ArrayList<Container_Merge> datuak = new ArrayList<>();
+    private final String[] ZUTABEAKIZENAK = {"ID CAN", "CAPACITY", "MEMBER", "START DATE","FINISH DATE"};
+
+
+    public CansTableModel() {
+        //datuak = model.showCans();
+    }
     
+    @Override
+    public Class getColumnClass(int c) {
+        return getValueAt(0, c).getClass();
+    }
+    
+    @Override
+    public int getColumnCount() {
+        return ZUTABEAKIZENAK.length;
+    }
+
+    
+    @Override
+    public String getColumnName(int col) {
+        return ZUTABEAKIZENAK[col];
+    }
+
+    
+    @Override
+    public int getRowCount() {
+        return datuak.size();
+    }
+ 
+    
+    @Override
+    public Object getValueAt(int row, int col) {
+        switch (col) {
+            case 0:
+                return datuak.get(row).getId();
+            case 1:
+                return datuak.get(row).getCapacity();
+            case 2:
+                return datuak.get(row).getEmail();
+            case 3:
+                return datuak.get(row).getDate();
+            case 4:
+                return datuak.get(row).getDate().plusDays(20);
+            default:
+                return null;
+        }
+    }
 }
