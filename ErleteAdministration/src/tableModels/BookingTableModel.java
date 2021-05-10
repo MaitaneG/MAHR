6e329208f4a575742ea0e68ba0099830d9537712
Moreno Rodriguez <moreno.manuel@uni.lan>
2021-05-10 08:56:22 +0200

    Booking

--- ErleteAdministration/src/tableModels/BookingTableModel.java
+++ ErleteAdministration/src/tableModels/BookingTableModel.java
@@ -5,16 +5,59 @@
  */
 package tableModels;
 
-import Classes.Accounts;
+import Classes.Extractor;
 import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
 import mvc.Model;
 
 /**
  *
  * @author USAURIO
  */
-public class BookingTableModel {
+public class BookingTableModel extends AbstractTableModel{
     private Model model = new Model();
-    private ArrayList<Accounts> datuak = new ArrayList<>();
+    private ArrayList<Extractor> datuak = new ArrayList<>();
     private final String[] ZUTABEAKIZENAK = {"ID", "DATE", "EMAIL"};
+
+
+    public BookingTableModel() {
+        //datuak = model.showUsers();
+    }
+    
+    @Override
+    public Class getColumnClass(int c) {
+        return getValueAt(0, c).getClass();
+    }
+    
+    @Override
+    public int getColumnCount() {
+        return ZUTABEAKIZENAK.length;
+    }
+
+    
+    @Override
+    public String getColumnName(int col) {
+        return ZUTABEAKIZENAK[col];
+    }
+
+    
+    @Override
+    public int getRowCount() {
+        return datuak.size();
+    }
+ 
+    
+    @Override
+    public Object getValueAt(int row, int col) {
+        switch (col) {
+            case 0:
+                return datuak.get(row).getId();
+            case 1:
+                return datuak.get(row).getDate();
+            case 2:
+                return datuak.get(row).getEmail();            
+            default:
+                return null;
+        }
+    }
 }
