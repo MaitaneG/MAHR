6bc7795aaf4626d989c6daf80d666e39962fe0dd
Ruben Santibanez <santibanez.ruben@uni.eus>
2021-05-12 10:46:45 +0200

    recuperacion

--- web/view/memberPanel.php
+++ web/view/memberPanel.php
@@ -15,46 +15,10 @@ if ($_SESSION["member"]) {
 
 <!doctype html>
 <html lang="es">
-<<<<<<< HEAD
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="styles/styles.css"> 
 
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <link href="https://fonts.googleapis.com/css2?family=Material+Icons+Outlined"
-    rel="stylesheet">
 
-    <title>Erlete beekepers' association</title>
-</head>
-
-<body class="url">
-    <!-- One way to present a title and content in a very prominent way on a page-->
-    <div class="jumbotron bg-yellow mb-0">
-        <h1>Erlete Beekepers' Association</h1>
-
-    </div>
-
-    <!--NAVIGATION BAR-->
-    <div class="container-fluid sticky-top mt-0">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light ">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav" >
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item">
-                        <a class="nav-link p-3" href="index.php">Home<span class="sr-only">(current)</span></a>
-                    </li>
-=======
     <head>
         <!-- Required meta tags -->
         <meta charset="utf-8">
@@ -94,20 +58,8 @@ if ($_SESSION["member"]) {
                         <li class="nav-item">
                             <a class="nav-link p-3" href="index.php">Home<span class="sr-only">(current)</span></a>
                         </li>
-                        <?php
-                        if ($member) {
-                            echo '<li class="nav-item active">
-                                        <a class="nav-link p-3 active disabled" href="#">Member Panel</a>
-                                    </li>';
-                        }
-                        ?>
 
-                        <li class="nav-item">
-                            <a class="nav-link p-3" href="contact.php">Contact</a>
-                        </li>
-                    </ul>
 
->>>>>>> f56ee3417e7152a801f3229192e9e6dc0b2f2d4b
                     <?php
                     if ($member) {
                         echo '<li class="nav-item active">
