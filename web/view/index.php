042e385d04ecfa9fdb16d6a891c48651321c2daa
Ruben Santibanez <santibanez.ruben@uni.eus>
2021-05-12 10:30:34 +0200

    recuperacion ´

---
+++ web/view/index.php
@@ -0,0 +1,177 @@
+<?php
+error_reporting(0);
+session_start();
+$logged = $_SESSION["submitted"];
+if ($_SESSION["member"]) {
+    $member = $_SESSION["member"];
+    $mail = $member[0]["mail"];
+    $admin = $member[0]["admin"];
+}
+?>
+
+<!doctype html>
+<html lang="es">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <link href="https://fonts.googleapis.com/css2?family=Material+Icons+Outlined"
+              rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="styles/styles.css"> 
+
+
+        <title>Erlete beekepers' association</title>
+    </head>
+    <body class="url">
+
+        <div class="jumbotron bg-yellow mb-0">
+            <h1>Erlete Beekepers' Association</h1>
+
+        </div>
+
+        <!--        NAVIGATION BAR-->
+        <div class="container-fluid sticky-top mt-0">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light ">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav" >
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link active p-3 disabled" href="#">Home<span class="sr-only">(current)</span></a>
+                        </li>
+                        <?php
+                        if ($member) {
+                            echo '<li class="nav-item">
+                                        <a class="nav-link p-3" href="memberPanel.php">Member Panel</a>
+                                    </li>';
+                        }
+                        ?>
+
+                        <li class="nav-item">
+                            <a class="nav-link p-3" href="contact.php">Contact</a>
+                        </li>
+                    </ul>
+
+                    <?php
+                    if (!$member) {
+
+
+                        echo '<form action="../controller/LoginValidation.php" class="form-inline" method="POST">
+
+                            <input name="email" type="email" class="form-control mb-2 mr-sm-2" 
+                                   size="30" required placeholder="Email">
+
+                            <input name="password" type="password" class="form-control mb-2 mr-sm-2" placeholder="Password" required/>
+
+                            <button type="submit" class="btn bg-yellow mb-2">Login</button>
+                        </form>';
+                    } else {
+                        echo "<h6 class='p-3'>$mail</h6>";
+                        echo '<form action="../controller/logout.php">'
+                        . '<input class="btn btn-danger" type="submit" value="Log Out"/>'
+                        . '</form>';
+                    }
+                    ?>
+
+                </div>
+            </nav>
+        </div>
+
+
+        <!--        CONTENIDO-->
+        <div class="container">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="images/img4.jpg" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="images/img1.jpeg" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="images/img2.jpg" alt="Third slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="images/img3.jpg" alt="Fourth slide">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-sm-12" align="center">
+                    <h2>What is Erlete Beekepers' Association?</h2>
+                    <p>Erlete Beekepers' Association is an association created to
+                        produce your own honey. For an annual fee of 30 euros it is possible to visit the honey extarction local, produce your own honey and take it home. Apart from the annual fee, the member will be required to pay 25 cents per kilo produced.</p>
+                    <p>To extract the honey you have to reserve the extractor. After you have used it, you will be authorised to use the honey extractor to store your honey for the next 20 days. </p>
+
+                    <p><b>Association Porpouse:</b>To serve the regional beekeepers, so that honey can be boarded as well as possible. The characteristics of care, problems and benefits of society.</p>
+                </div>
+                 
+                <div class="col-sm-12 mt-3" align="center"><h2>Rules</h2></div>
+                <div class="col-sm-4">
+
+                    <h3>1. When they bring honey: </h3>
+                    <p>- Don't bring a bee to the honey.<br>
+                       - The doors of the window and car were always locked.<br>
+                       - Once the car's gone, take the car to the parking lot.<br>
+                       - Leave no sign of honey on the porch, keep it clean.<br>
+                       - "The straws do not leave [them] on the ground, but on the foundations prepared.</p>
+                </div>
+                <div class="col-sm-4">
+
+                    <h3>2. Extracting honey: </h3>
+                    <p>- Don't bring a bee to the honey.<br>
+                       - After inserting the honey that was produced into the madurator, prefix it to the producer's name, name, and date.<br>
+                       - Fill out a file containing data from the honey campaign.<br>
+                       - Make your shop clean and clean for the next one.</p>
+                </div>
+                <div class="col-sm-4">
+
+                    <h3>3. The ship of honey: </h3>        
+                    <p>- It will be made in less than 20 days after the honey is extracted.<br>
+                       - Three samples or muestra must be left for each jar of honey.<br>
+                       - Fill in the section corresponding to the launch of the honey chip in the honey campaign.</p>
+                </div>
+
+                <br>
+                <div class="col-sm-12" align="center"><h2>Bees Information</h2></div>
+
+
+            </div>
+
+
+
+
+
+        </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+    </body>
+</html>
---
+++ web/view/memberPanel.php
@@ -0,0 +1,364 @@
+<?php
+error_reporting(0);
+session_start();
+
+$logged = $_SESSION["submitted"];
+if ($_SESSION["member"]) {
+    $member = $_SESSION["member"];
+    $mail = $member[0]["mail"];
+    $admin = $member[0]["admin"];
+}else{
+    echo "RESTRICTED AREA";
+    die();
+}
+?>
+
+<!doctype html>
+<html lang="es">
+<<<<<<< HEAD
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="styles/styles.css"> 
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link href="https://fonts.googleapis.com/css2?family=Material+Icons+Outlined"
+    rel="stylesheet">
+
+    <title>Erlete beekepers' association</title>
+</head>
+
+<body class="url">
+    <!-- One way to present a title and content in a very prominent way on a page-->
+    <div class="jumbotron bg-yellow mb-0">
+        <h1>Erlete Beekepers' Association</h1>
+
+    </div>
+
+    <!--NAVIGATION BAR-->
+    <div class="container-fluid sticky-top mt-0">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light ">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav" >
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link p-3" href="index.php">Home<span class="sr-only">(current)</span></a>
+                    </li>
+=======
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+ 
+
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <link href="https://fonts.googleapis.com/css2?family=Material+Icons+Outlined"
+              rel="stylesheet">
+               <link rel="stylesheet" type="text/css" href="styles/styles.css"> 
+
+        <title>Erlete beekepers' association</title>
+    </head>
+
+    <body class="url">
+        <!-- One way to present a title and content in a very prominent way on a page-->
+        <div class="jumbotron bg-yellow mb-0">
+            <h1>Erlete Beekepers' Association</h1>
+
+        </div>
+
+        <!--NAVIGATION BAR-->
+        <div class="container-fluid sticky-top mt-0">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light ">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav" >
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link p-3" href="index.php">Home<span class="sr-only">(current)</span></a>
+                        </li>
+                        <?php
+                        if ($member) {
+                            echo '<li class="nav-item active">
+                                        <a class="nav-link p-3 active disabled" href="#">Member Panel</a>
+                                    </li>';
+                        }
+                        ?>
+
+                        <li class="nav-item">
+                            <a class="nav-link p-3" href="contact.php">Contact</a>
+                        </li>
+                    </ul>
+
+>>>>>>> f56ee3417e7152a801f3229192e9e6dc0b2f2d4b
+                    <?php
+                    if ($member) {
+                        echo '<li class="nav-item active">
+                        <a class="nav-link p-3 active disabled" href="#">Member Panel</a>
+                        </li>';
+                    }
+                    ?>
+
+                    <li class="nav-item">
+                        <a class="nav-link p-3" href="contact.php">Contact</a>
+                    </li>
+                </ul>
+
+                <?php
+                if (!$member) {
+
+
+                    echo '<form action="../controller/LoginValidation.php" class="form-inline" method="POST">
+
+                    <input name="email" type="email" class="form-control mb-2 mr-sm-2" 
+                    size="30" required placeholder="Email">
+
+                    <input name="password" type="password" class="form-control mb-2 mr-sm-2" placeholder="Password" required/>
+
+                    <button type="submit" class="btn bg-yellow mb-2">Login</button>
+                    </form>';
+                } else {
+                    echo "<h6 id='currentMail' class='p-3'>$mail</h6>";
+                    echo '<form action="../controller/logout.php">'
+                    . '<input class="btn btn-danger" type="submit" value="Log Out"/>'
+                    . '</form>';
+                }
+                ?>
+
+            </div>
+        </nav>
+    </div>
+    <!-- END OF THE NAVIGATION BAR -->
+
+    <!-- CONTAINER-->
+    <div class="container">
+
+        <!-- ACCORDION-->
+        <div id="accordion" class="mt-3">
+            <div class="card">
+                <div class="card-header" id="headingOne">
+                    <h5 class="mb-0">
+                        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            1. Bookings
+                        </button>
+                    </h5>
+                </div>
+                <div id="collapseOne" class="collapse " aria-labelledby="headingOne" data-parent="#accordion">
+                    <div class="card-body">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-12">
+
+                                    <!--DATEPICKER using JQuery to show a calendar-->
+                                    <p>Date: <input type="text" id="datepicker" size="30"></p>
+
+
+                                </p><button id="button-reserve" class="btn bg-yellow ml-3" type="button">Reserve</button>
+
+
+
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <!-- Table for the bookings-->
+                            <h3>Bookings</h3>
+                            <table  class="table">
+                                <thead>
+                                    <tr>
+
+                                        <th>Date</th>
+                                        <th colspan="2">Email</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="table-booking">
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header" id="headingTwo">
+            <h5 class="mb-0">
+                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    2. Cans
+                </button>
+            </h5>
+        </div>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+            <div class="card-body p-3">
+                <h3>Production</h3> 
+                <div class="row p-3">
+
+                    <input type="text" name="Production" placeholder="Production Kg"> 
+                    <button class="btn bg-yellow ml-3" type="button" >Register</button>
+                    <label></label>
+                </div>
+                <!--CARD to divide the cans-->
+                <div class="row justify-content-center">
+                    <div class="col-6 col-md-4 col-lg-3 my-3 cans-cards">
+                        <div class="card">
+                            <h5 class="card-title" align="center">Can 1 100L</h5>
+
+                            <div class="card-body">
+                                <img class="card-img-top img" src="images/can.png" alt="honey can">
+
+                                <p class="card-text">
+                                    Start use date: 15/10/2021 <br>
+                                    End use date: 20/11/2021
+                                </p>
+                                <button class="btn bg-yellow">Use</button> 
+                                <button class="btn bg-yellow">Cancel</button> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header" id="headingThree">
+                <h5 class="mb-0">
+                    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        3. Payments
+                    </button>
+                </h5>
+            </div>
+            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                <div class="card-body">
+                    <div class="row">
+                        <!--Table to see the payments-->
+                        <div class="col-12">
+                            <h3>View</h3>
+                            <table class="table">
+                                <thead>
+                                    <tr>
+
+                                        <th scope="col">Date</th>
+                                        <th scope="col">Amount</th>
+                                        <th scope="col">Concept</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row"></th>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+
+                                </tbody>
+                            </table>
+
+                        </div>
+                    </div>
+
+
+                    <div class="row">
+                        <!--Table to see the fees and to pay them-->
+                        <div class="col-6">
+                            <h3>Fees</h3>
+                            <table class="table">
+                                <thead>
+                                    <tr>
+
+                                        <th scope="col">ID_Fee</th>
+                                        <th scope="col">Year</th>
+                                        <th scope="col">Payed</th>
+
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row"></th>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+
+                                </tbody>
+                            </table>
+                            <button class="btn bg-yellow" type="button">Pay</button>
+
+                        </div>
+
+                        <!--Table to see the fees and to pay them-->
+                        <div class="col-6">
+                            <h3>Tax</h3>
+                            <table class="table">
+                                <thead>
+                                    <tr>
+
+                                        <th scope="col">Production</th>
+                                        <th scope="col">Date</th>
+                                        <th scope="col">Amount</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row"></th>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+
+                                </tbody>
+                            </table>
+                            <button class="btn bg-yellow" type="button">Pay</button>
+
+                        </div>
+                    </div>
+
+
+
+
+
+
+                </div>
+            </div>
+            <!--ACCORION END-->
+        </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+        <script src="../controller/app.js"></script>
+
+        <!--The function to make uit work the datapicker-->
+        <script>
+            $(function () {
+
+
+                $("#datepicker").datepicker();
+                $("#format").on("change",
+                    function () {
+                        $("#datepicker").datepicker("option", "dateFormat", $(this).val());
+
+                    });
+
+            });
+
+        </script>
+    </body>
+    </html>
