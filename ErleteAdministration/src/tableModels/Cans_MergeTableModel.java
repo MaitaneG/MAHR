07a7fd8cf3163cd2e1008414a845d783b4dbf2ec
Gallastegui Juan <gallastegui.maitane@uni.lan>
2021-05-14 08:32:31 +0200

    Vamo

--- ErleteAdministration/src/tableModels/Cans_MergeTableModel.java
+++ ErleteAdministration/src/tableModels/Cans_MergeTableModel.java
@@ -58,7 +58,7 @@ public class Cans_MergeTableModel extends AbstractTableModel {
     public String getColumnName(int col) {
         return ZUTABEAKIZENAK[col];
     }
-
+     
     /**
      * 
      * @return how many rows the table has
