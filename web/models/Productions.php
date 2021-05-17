09ff36610c371109993cf4d1371335bd6fe4a35f
Ruben Santibanez <santibanez.ruben@uni.eus>
2021-05-17 11:56:16 +0200

    account movement tables is functional

--- web/models/Productions.php
+++ web/models/Productions.php
@@ -94,6 +94,29 @@ function selectPendentTaxes($mail) {
     return $results;
 }
 
+function selectPayedTaxes($mail) {
+    //Select reserve of a selected day
+    //return: String
+    $conexion = ConnectDataBase();
+    $query = "SELECT * FROM productions WHERE mail='$mail' AND PAYED=1";
+
+    $result = mysqli_query($conexion, $query);
+    if (!$result) {
+        die("Query error" . mysqli_error($conexion));
+    }
+    $results = array();
+    while ($row = mysqli_fetch_array($result)) {
+        $results[] = array(
+            'date' => $row[2],
+            'tax' => $row[4],
+            'payed' => $row[5]
+        );
+    }
+
+    mysqli_close($conexion);
+    return $results;
+}
+
 function editProduction($mail) {
     //edit payed state
     //return String
