d601745eae7d04e1b16eb561ea792fe5147d0b8e
Gallastegui Juan <gallastegui.maitane@uni.lan>
2021-05-17 08:33:34 +0200

    Libraries

--- ErleteAdministration/test/TestUser.java
+++ ErleteAdministration/test/TestUser.java
@@ -1,3 +1,6 @@
+
+import junit.framework.TestCase;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,6 +11,6 @@
  *
  * @author gallastegui.maitane
  */
-public class TestUser {
+public class TestUser extends TestCase{
     
 }
