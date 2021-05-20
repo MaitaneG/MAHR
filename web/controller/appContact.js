0065254a8b937dc5f86a7d4b86ad27b25156cd50
Ruben Santibanez <santibanez.ruben@uni.eus>
2021-05-20 11:42:26 +0200

    consulta de miembros y ajax preparados para añadir a contact

---
+++ web/controller/appContact.js
@@ -0,0 +1,40 @@
+$(function () {
+    
+    /*
+     * VISUALIZE ALL MEMBERS FUNCTION
+     */
+    function fecthMembers() {
+        
+        let fetch=1;
+        let url = "../controller/membersC.php";
+
+        $.post(url, {fetch}, function (response) {
+            
+            if (response) {
+          
+                let members = JSON.parse(response);
+                template = "";
+                 members.forEach(member => {
+                     template+=`            
+                                                <li>${member.name} ${member.surname}</li>`;
+                 });
+                                    
+
+                 
+                $("#members-list").html(template);
+                $("#members-list").show();
+
+
+            } else {
+                $("#members-list").html("");
+                
+            }
+        });
+    }
+    ;
+    
+    
+
+    
+    
+});
--- web/models/Members.php
+++ web/models/Members.php
@@ -1,13 +1,46 @@
 <?php
 
 include("testConexion.php");
-
+/*
+ * API MEMBERS
+ */
 function searchMember($mailP, $passwordP) {
     //Select one member
     //return: 
     //0-dni 1-name 2-surname 3-mail 4-password 5-account 6-admin
     $conexion = ConnectDataBase();
     $query = "SELECT * FROM members WHERE mail='$mailP' and password='$passwordP'";
+
+
+    $result = mysqli_query($conexion, $query);
+    if (!$result) {
+        die("Query error" . mysqli_error($conexion));
+    }
+    $results = array();
+    while ($row = mysqli_fetch_array($result)) {
+        $results[] = array(
+            'dni' => $row[0],
+            'name' => $row[1],
+            'surname' => $row[2],
+            'mail' => $row[3],
+            'password' => $row[4],
+            'account' => $row[5],
+            'admin' => $row[6],
+            'active' => $row[7]
+        );
+    }
+
+    mysqli_close($conexion);
+    return $results;
+}
+
+
+function searchMembers() {
+    //Select active members
+    //return: 
+    //0-dni 1-name 2-surname 3-mail 4-password 5-account 6-admin
+    $conexion = ConnectDataBase();
+    $query = "SELECT * FROM members where active=1";
 
 
     $result = mysqli_query($conexion, $query);
