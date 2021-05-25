a573a44ba90b4462e1a9c9b0e916f14e42995fdb
Ruben Santibanez <santibanez.ruben@uni.eus>
2021-05-25 18:44:17 +0200

    pruebas con captcha

--- web/view/index.php
+++ web/view/index.php
@@ -31,6 +31,8 @@ if ($_SESSION["member"]) {
 
         <!--Link icono pestaña -->
         <link rel="shortcut icon" href="images/Iconopestaña.png" />
+      
+        
 
         <title>Erlete beekepers' association</title>
     </head>
@@ -93,12 +95,14 @@ if ($_SESSION["member"]) {
                             </span>Login</button>
 							</form>';
                     } else {
-                       echo "<h6 class='p-3'>$mail</h6>";
+                        echo "<h6 class='p-3'>$mail</h6>";
                         echo '<form action="../controller/Logout.php">'
                         . '<button class="btn btn-danger" type="submit"><span class="material-icons-outlined">
                         logout
                         </span>Log Out</button>'
-                        . '</form>';
+                        . '</form>'
+                                ;
+                        
                     }
                     ?>
 
@@ -262,33 +266,33 @@ if ($_SESSION["member"]) {
                 <div class="col-lg-4 col-md-6 my-2">
                     <!-- Copyright -->
                     <a href="mailto:{email}?subject={subject}&body={content}">
-                        
-                    
-                    <span class="material-icons-outlined text-light">
-                        mail
-                    </span>Send us an email 
+
+
+                        <span class="material-icons-outlined text-light">
+                            mail
+                        </span>Send us an email 
                     </a>
                 </div>
 
                 <div class="col-lg-4 col-md-6 my-2">
                     <!-- Copyright -->
                     <a href="tel:{phone}">
-                        
-                  
-                    <span class="material-icons-outlined text-light">
-                        phone
-                    </span> Call us 
-                      </a>
+
+
+                        <span class="material-icons-outlined text-light">
+                            phone
+                        </span> Call us 
+                    </a>
                 </div>
 
                 <div class="col-lg-4 col-md-6 my-2">
                     <!-- Copyright -->
                     <a href="sms:{phone}?body={content}">
-                        
-                    
-                    <span class="material-icons-outlined text-light">
-                        sms
-                    </span>   Send us a message 
+
+
+                        <span class="material-icons-outlined text-light">
+                            sms
+                        </span>   Send us a message 
                     </a> 
                 </div>
 
@@ -308,6 +312,9 @@ if ($_SESSION["member"]) {
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+       
+
+
 
     </body>
 </html>				
