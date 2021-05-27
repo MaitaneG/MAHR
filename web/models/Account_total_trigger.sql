73e0812cfe971cae7a05d091e87b30e18ac69d35
Ruben Santibanez <santibanez.ruben@uni.eus>
2021-05-27 13:51:20 +0200

    actualizar triggers

--- web/models/Account_total_trigger.sql
+++ web/models/Account_total_trigger.sql
@@ -1,24 +1,23 @@
-CREATE TRIGGER Tr_total
- AFTER INSERT OF amount ON ACCOUNT
-FOR EACH ROW
-BEGIN
-   SET last_Total FLOAT;
-    SELECT TOTAL INTO last_Total
-    FROM ACCOUNT 
-    ORDER by ID_MOVE DESC
-    LIMIT 1;
+CREATE TRIGGER `Tr_total` BEFORE INSERT ON `account`
+ FOR EACH ROW BEGIN 
+    declare last_Total FLOAT; 
+    SELECT TOTAL INTO last_Total FROM account ORDER by ID_MOVE DESC LIMIT 1; 
 
-    IF OLD.COLLECTOR="admin@erlete.eus"
-        UPDATE ACCOUNT
-        SET TOTAL=last_Total+OLD.AMOUNT
-        WHERE ID=OLD.ID_MOVE;
-    ELSE
-        UPDATE ACCOUNT
-        SET TOTAL=last_Total-OLD.AMOUNT
-        WHERE ID=OLD.ID_MOVE;
-        END IF;
-END;
-/
+    IF (new.COLLECTOR="admin@erlete.eus") then 
+         set new.total = last_Total+new.amount;
 
+    ELSE set new.total = last_Total-new.amount;
 
+    END IF; 
+END
 
+CREATE TRIGGER `new_can` AFTER INSERT ON `cans`
+ FOR EACH ROW begin
+	insert into account(payer,collector,date,amount,concept)
+	values ("admin@erlete.eus","cans@cans.com",curdate(),new.price,"Buy a new can");
+end
+
+CREATE TRIGGER `new_fee` AFTER INSERT ON `members`
+ FOR EACH ROW begin
+	insert into fees(year,mail,amount) values (year(now()),new.mail,30);
+end
